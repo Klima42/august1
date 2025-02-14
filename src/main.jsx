b3bb84c440fe747d28d83f6db32f5800bc23d140
Alice Leiser <csurcaune@gmail.com>
2025-02-14 15:52:25 +0100

    idk

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import homepage from './homepage';
+import Homepage from './Homepage';
 import AIChatAssistant from './AIChatAssistant';
 import DeepChef from './deepchef';
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/chat" element={
           <div className="container mx-auto px-4 py-8 max-w-4xl">
             <AIChatAssistant />
